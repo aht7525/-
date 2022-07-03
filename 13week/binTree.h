53d9726335177f62084395c8e1b88840ee2e0f76
Nondurung <33015799+aht7525@users.noreply.github.com>
2022-07-03 15:00:21 +0900

    Delete 13week directory

--- 13week/binTree.h
+++
@@ -1,9 +0,0 @@
-struct tnode {
-    char *word;
-    int count;
-    struct tnode *left;
-    struct tnode *right;
-};
-
-struct tnode *addTree(struct tnode *p, char *w);
-void treePrint(struct tnode *p);
